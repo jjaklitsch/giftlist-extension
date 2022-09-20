e64a41eb720c53d705a6f560622a086828d2eafa
DESKTOP-G7OOOLS\webap <>
2022-09-20 03:55:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { changeColor } from './main';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={changeColor}>
+          Change color
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
